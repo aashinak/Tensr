b64e793056c3216790bb90c0048b90a82c06bf51
Aashin A K <aashin9605@gmail.com>
2025-06-09 12:25:51 +0530

    feat: enhance SignupForm with loading state and OTP dialog integration

--- client/src/modules/auth/signup/components/SignupForm.tsx
+++ client/src/modules/auth/signup/components/SignupForm.tsx
@@ -1,3 +1,7 @@
+// Used to create a signup form with validation and OTP dialog
+// Uses react-hook-form for form handling and zod for validation.
+// Displays input fields for username, fullname, email, password, and confirm password.
+
 "use client";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
@@ -6,6 +10,7 @@ import InputBox from "@/shared/components/InputBox";
 import Button from "@/shared/components/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signupSchema } from "../validators";
+import OtpDialog from "../../shared/components/OtpDialog";
 
 function SignupForm() {
   const {
@@ -13,68 +18,88 @@ function SignupForm() {
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInput>({ resolver: zodResolver(signupSchema) });
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsOpen(true);
+      setIsLoading(false);
+    }, 2000);
+  };
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-2 gap-3 md:gap-5 w-full"
-    >
-      {/* Row 1: Username & Fullname */}
-      <InputBox
-        onChangeCallback={(value) => {
-          console.log(value);
-        }}
-        required
-        name="username"
-        placeholder="Enter your username"
-        register={register}
-        errors={errors}
-      />
-      <InputBox
-        required
-        name="fullname"
-        placeholder="Enter your fullname"
-        register={register}
-        errors={errors}
-      />
+    <div className="w-full">
+      <OtpDialog isOpen={isOpen} setIsOpen={setIsOpen} />
 
-      {/* Row 2: Email (spans both columns) */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-2 gap-3 md:gap-5 w-full"
+      >
+        {/* Row 1: Username & Fullname */}
+        <InputBox
+          disabled={isLoading}
+          onChangeCallback={(value) => {
+            console.log(value);
+          }}
+          required
+          name="username"
+          placeholder="Enter your username"
+          register={register}
+          errors={errors}
+        />
+        <InputBox
+          disabled={isLoading}
+          required
+          name="fullname"
+          placeholder="Enter your fullname"
+          register={register}
+          errors={errors}
+        />
 
-      <InputBox
-        className="col-span-2"
-        required
-        type="email"
-        name="email"
-        placeholder="Enter your email"
-        register={register}
-        errors={errors}
-      />
+        {/* Row 2: Email (spans both columns) */}
 
-      {/* Row 3: Password & Confirm Password */}
-      <InputBox
-        required
-        name="password"
-        type="password"
-        placeholder="Enter your password"
-        register={register}
-        errors={errors}
-      />
-      <InputBox
-        required
-        name="confirmPassword"
-        type="password"
-        placeholder="Confirm password"
-        register={register}
-        errors={errors}
-      />
+        <InputBox
+          disabled={isLoading}
+          className="col-span-2"
+          required
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          register={register}
+          errors={errors}
+        />
 
-      {/* Submit Button */}
-      <Button
-        className="col-span-2 font-semibold bg-[#d3d3d3] text-black"
-        text="Sign Up"
-        type="submit"
-      />
-    </form>
+        {/* Row 3: Password & Confirm Password */}
+        <InputBox
+          disabled={isLoading}
+          required
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          register={register}
+          errors={errors}
+        />
+        <InputBox
+          disabled={isLoading}
+          required
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm password"
+          register={register}
+          errors={errors}
+        />
+
+        {/* Submit Button */}
+        <Button
+          disabled={isLoading}
+          className="col-span-2 font-semibold bg-[#d3d3d3] text-black"
+          text={isLoading ? "Signing up..." : "Sign Up"}
+          type="submit"
+        />
+      </form>
+    </div>
   );
 }
 
