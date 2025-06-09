1d4ace63cdfdbfcb2dd846ea1c7b7e7bbef6611a
Aashin A K <aashin9605@gmail.com>
2025-06-09 12:02:34 +0530

    feat: add Loading component for improved user experience during data fetching

---
+++ client/src/shared/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center h-dvh">
+      <div className="w-14 h-14 border-4 border-[#454545] border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loading;
