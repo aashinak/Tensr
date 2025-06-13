c821b62366ffff1595fa3ee12afc16da3db223d1
Aashin A K <aashin9605@gmail.com>
2025-06-13 12:27:19 +0530

    feat: create layout component for reset password page

---
+++ client/src/app/(forgotPassVerification)/reset-password/layout.tsx
@@ -0,0 +1,25 @@
+import Typography from "@/shared/components/Typography";
+import Link from "next/link";
+import React from "react";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+function layout({ children }: LayoutProps) {
+  return (
+    <div className="h-dvh flex flex-col items-center gap-4 md:gap-10 pt-5 md:pt-10">
+      <Link className="z-50" href={"/"}>
+        <Typography
+          className="bg-[#272727] px-6 py-2 rounded-2xl"
+          variant="tensr"
+        >
+          Tensr
+        </Typography>
+      </Link>
+      {children}
+    </div>
+  );
+}
+
+export default layout;
