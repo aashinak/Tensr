dd06c632fa5c5b3293073cbe1d40400043e6f6a3
Aashin A K <aashin9605@gmail.com>
2025-06-13 13:37:15 +0530

    feat: enhance password validation schema to enforce complexity requirements

--- client/src/modules/auth/signin/validators.ts
+++ client/src/modules/auth/signin/validators.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const usernameRegex = /^[a-zA-Z0-9_]+$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^])[A-Za-z\d@$!%*?#&^]{8,}$/;
 
 export const signInSchema = z.object({
   identifier: z
@@ -12,7 +14,14 @@ export const signInSchema = z.object({
     .refine((val) => usernameRegex.test(val) || emailRegex.test(val), {
       message: "Enter a valid username or email",
     }),
-  password: z.string().trim().min(8, "Password must be at least 8 characters"),
+  password: z
+    .string()
+    .trim()
+    .min(8, "Password must be at least 8 characters")
+    .regex(
+      passwordRegex,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
 });
 
 export const forgotEmailSchema = z.object({
